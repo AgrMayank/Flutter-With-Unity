6f9187efba08cd46db61355a8dae3b6169fdf64b
Mayank Raj <AgrMayank@outlook.com>
2022-03-03 20:28:31 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'menu_screen.dart';
 import 'unity_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
